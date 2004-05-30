00de63b0251770ecb27f301b771511c6fdbc2641
Paul Fenwick <pjf@perltraining.com.au>
2004-05-30 04:29:51 +0000

    More documentation.

--- UID.pm
+++ UID.pm
@@ -4,7 +4,13 @@ Proc::UID - Manipulate a variety of UID and GID settings.
 
 =head1 SYNOPSIS
 
-	use Proc::UID;
+	use Proc::UID qw(:vars);
+	print "My saved-uid is $SUID, my real-uid is $RUID\n";
+
+	use Proc::UID qw(setuid_permanent);
+	print "Permanently dropping privileges to $new_uid\n";
+	setuid_permanent($new_uid); # Throws an exception on failure.
+
 
 =head1 WARNING
 
@@ -12,15 +18,37 @@ This release of Proc::UID is for testing and review purposes only.
 Please do not use it in production code.  The interface may change,
 and the underlying code has not yet been rigourously tested.
 
+If you discover any of the included tests fail, or that any other
+problems with the code, please contact Paul Fenwick, pjf@cpan.org.
+
 =head1 DESCRIPTION
 
-Perl only has concepts of effective and real UIDs, whereas a
-number of operating systems have further concepts, such as saved
-UIDs.  This module is intended to provide a way for those additional
-UIDs to be manipulated.
+Perl only has concepts of effective and real user-ids (UIDs) and 
+group-ids (GIDs), accessible via the special variables $<, $>, $( and $).
+However most modern Unix systems also have a concept of saved UIDs.
 
-A number of functions are provided to perform logical operations,
-such as irrevocably dropping privileges.
+This module provides a consistent and logical interface to real,
+effective, and saved UIDs and GIDs.  It also provides a way to
+permanently drop privileges to that of a given user, a process
+which '$< = $> = $uid' does not guarantee, and the exact syntax
+of which may vary from between operating systems.
+
+Proc::UID is also very pedantic about making sure that operations
+succeeded, and checking the value which it returns for a UID/GID
+really is the one that's being used.  Perl may sometimes cache
+the values of $<, $>, $( and $), which means they can be wrong
+after being changed with low-level system calls.
+
+Proc::UID provides both a variable and function interfaces to
+underlying UIDs.
+
+=head2 VARIABLE INTERFACE
+
+	To be compelted.
+
+=head2 FUNCTIONAL INTERFACE
+
+	To be completed.
 
 =head1 BUGS
 
